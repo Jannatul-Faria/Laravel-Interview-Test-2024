c5a56e80c6e217cf1b1b1c58832fbc921854fe38
Jannatul-faria <kairasana8@gmail.com>
2024-12-19 11:38:16 +0600

    logic-build

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -2,13 +2,93 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
+use App\Models\State;
 use App\Models\Country;
 use Illuminate\Http\Request;
 
 class LocationController extends Controller
 {
-    public function countriesList(){
+
+    // Country CRUD
+    public function getCountries()
+    {
         return response()->json(Country::with('states')->get());
     }
-    
+
+    public function storeCountry(Request $request)
+    {
+        $request->validate(['name' => 'required|string|max:255']);
+        $country = Country::create($request->all());
+        return response()->json(['success' => true, 'country' => $country]);
+    }
+
+    public function updateCountry(Request $request, $id)
+    {
+        $request->validate(['name' => 'required|string|max:255']);
+        $country = Country::findOrFail($id);
+        $country->update($request->all());
+        return response()->json(['success' => true, 'country' => $country]);
+    }
+
+    public function deleteCountry($id)
+    {
+        Country::destroy($id);
+        return response()->json(['success' => true]);
+    }
+
+    // State CRUD
+    public function getStates()
+    {
+        return response()->json(State::with('country', 'cities')->get());
+    }
+
+    public function storeState(Request $request)
+    {
+        $request->validate(['name' => 'required', 'country_id' => 'required|exists:countries,id']);
+        $state = State::create($request->all());
+        return response()->json(['success' => true, 'state' => $state]);
+    }
+
+    public function updateState(Request $request, $id)
+    {
+        $request->validate(['name' => 'required', 'country_id' => 'required|exists:countries,id']);
+        $state = State::findOrFail($id);
+        $state->update($request->all());
+        return response()->json(['success' => true, 'state' => $state]);
+    }
+
+    public function deleteState($id)
+    {
+        State::destroy($id);
+        return response()->json(['success' => true]);
+    }
+
+    // City CRUD
+    public function getCities()
+    {
+        return response()->json(City::with('state')->get());
+    }
+
+    public function storeCity(Request $request)
+    {
+        $request->validate(['name' => 'required', 'state_id' => 'required|exists:states,id']);
+        $city = City::create($request->all());
+        return response()->json(['success' => true, 'city' => $city]);
+    }
+
+    public function updateCity(Request $request, $id)
+    {
+        $request->validate(['name' => 'required', 'state_id' => 'required|exists:states,id']);
+        $city = City::findOrFail($id);
+        $city->update($request->all());
+        return response()->json(['success' => true, 'city' => $city]);
+    }
+
+    public function deleteCity($id)
+    {
+        City::destroy($id);
+        return response()->json(['success' => true]);
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ Route::get('/form', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->name('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+// })->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -25,12 +25,32 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Route::resource('countries', CountryController::class);
-// Route::resource('states',StateController::class);
-// Route::resource('cities', CityController::class);
+Route::resource('countries', CountryController::class);
+Route::resource('states',StateController::class);
+Route::resource('cities', CityController::class);
 
 Route::controller(LocationController::class)->group(function(){
-    //country crud
+    
    Route::get('/countries_list', 'countriesList' );
 });
+
+// Route::controller(LocationController::class)->group(function () {
+//     // Country CRUD
+//     Route::get('/countries', 'getCountries');
+//     Route::post('/countries', 'storeCountry');
+//     Route::put('/countries/{id}', 'updateCountry');
+//     Route::delete('/countries/{id}', 'deleteCountry');
+
+//     // State CRUD
+//     Route::get('/states', 'getStates');
+//     Route::post('/states', 'storeState');
+//     Route::put('/states/{id}', 'updateState');
+//     Route::delete('/states/{id}', 'deleteState');
+
+//     // City CRUD
+//     Route::get('/cities', 'getCities');
+//     Route::post('/cities', 'storeCity');
+//     Route::put('/cities/{id}', 'updateCity');
+//     Route::delete('/cities/{id}', 'deleteCity');
+// });
 require __DIR__.'/auth.php';
